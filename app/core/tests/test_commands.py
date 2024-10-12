f88dbdc7516e61c42700e43977128d9b1bec2006
root <lseirina87@gmail.com>
2024-10-12 19:47:52 +0300

    Added test_wait fot db delay

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,7 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Tests for wait_for_db command."""
-    
+
     def test_wait_for_db_ready(self, patched_check):
         """Test wait_for_db commanf if databae ready."""
         patched_check.return_value = True
@@ -21,3 +21,14 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_time, patched_check):
+        """"Test for wait for db with OperationalError"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.call_with(databases=['default'])
