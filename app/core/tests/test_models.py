f6f1ea5811b4c8d3e027503037f9652ad709c2f6
root <lseirina87@gmail.com>
2024-10-14 14:23:31 +0300

    Added setUp to test userTestResult

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,20 +2,31 @@
 Tests for models.
 """
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 from core import models
 
 
 class ModelTests(TestCase):
 
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='Test Name',
+            password='Test123',
+        )
         self.test_set = models.TestSet.objects.create(
             title='Test Title',
-            text='Test Text',
+            description='Test Text',
         )
         self.question = models.Question.objects.create(
             test_set=self.test_set,
             text='Test Text',
+        )
+        self.user_test_result = models.UserTestResult.objects.create(
+            user=self.user,
+            test_set=self.test_set,
+            score=80,
+            total_questions=10,
+            correct_answers=8,
         )
 
     def test_create_test_set(self):
