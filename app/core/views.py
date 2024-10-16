adef4c421208dd4cbe0d0ac98f6311e274c22427
root <lseirina87@gmail.com>
2024-10-16 12:25:13 +0300

    Added test_result view

--- app/core/views.py
+++ app/core/views.py
@@ -9,7 +9,7 @@ from core.forms import QuestionForm
 @login_required
 def start_test(request, test_set_id):
     test_set = get_object_or_404(TestSet, id=test_set_id)
-    questions = test_set.questions.all()
+    questions = test_set.question.all()
     if 'current_question' not in request.session:
         request.session['current_question'] = 0
         request.session['score'] = 0
@@ -35,3 +35,29 @@ def start_test(request, test_set_id):
     return render(
         request, 'test.html', {'question': current_question, 'form': form}
         )
+
+
+@login_required
+def test_result(request, test_set_id):
+    test_set = get_object_or_404(TestSet, id=test_set_id)
+    total_questions = test_set.question.count()
+    correct_answers = request.session['score']
+    percentage = (correct_answers / total_questions) * 100
+
+    UserTestResult.objects.create(
+        user=request.user,
+        test_set=test_set,
+        score=correct_answers,
+        total_questions=total_questions,
+        correct_answers=correct_answers
+    )
+
+    del request.session['current_question']
+    del request.session['score']
+
+    return render(request, 'result.html', {
+        'test_set': test_set,
+        'correct_answers': correct_answers,
+        'total_questions': total_questions,
+        'percentage': percentage,
+    })
